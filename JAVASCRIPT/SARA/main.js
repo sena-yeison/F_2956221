94a5b20232caba5af3b868f1b314bfef1fead83c
Yeison Barrios F <yeison.barrios.funieles@gmail.com>
2024-08-13 11:20:13 -0500

    ok

--- JAVASCRIPT/SARA/main.js
+++ JAVASCRIPT/SARA/main.js
@@ -5,7 +5,8 @@ let ListadoProgramas = [];
 // Función de inicio
 const start = () => {
   let opcionStart = 0;
-  let opcionAprendices = 0;
+  let opcionAprendices = 0; 
+  let opcionActualizacionAprendices = 0;
   do {
     opcionStart = parseInt(
       prompt(`Bienvenido a SARA 
@@ -19,8 +20,8 @@ const start = () => {
     switch (opcionStart) {
       case 1:
         do {
-            opcionAprendices = parseInt(
-              prompt(`Bienvenido a SARA 
+          opcionAprendices = parseInt(
+            prompt(`Bienvenido a SARA 
                     Sistema Automatizado de Registro de Aprendices
                     
                     1. Registro
@@ -28,22 +29,22 @@ const start = () => {
                     3. Actualización de aprendiz
                     4. Anular matricula del aprendiz
                     5. Salir`)
-            );
-        
-            switch (opcionAprendices) {
-              case 1:
-                RegistroAprendiz();
-                break;
-              case 2:
-                listadoTotalAprendices();
-                break;
-              case 3:
-                ActualizarDatosAprendiz()
-                break;
-              default:
-                break;
-            }
-          } while (opcionAprendices != 5);
+          );
+
+          switch (opcionAprendices) {
+            case 1:
+              RegistroAprendiz();
+              break;
+            case 2:
+              listadoTotalAprendices();
+              break;
+            case 3:
+              ActualizarDatosAprendiz();
+              break;
+            default:
+              break;
+          }
+        } while (opcionAprendices != 5);
         break;
       case 2:
         break;
@@ -55,56 +56,50 @@ const start = () => {
   } while (opcionStart != 3);
 };
 
-
-
-
 /*
 ----------------------------------------------
 METODOS O FUNCIONES
 ----------------------------------------------
-*/ 
-
+*/
 
 // METODOS DEL APRENDIZ
 
 // REGISTRO
-function RegistroAprendiz(){
-    const cantidadAprendiz = parseInt(prompt(`¿Cuántos aprendices desea registrar?`));
+function RegistroAprendiz() {
+  const cantidadAprendiz = parseInt(
+    prompt(`¿Cuántos aprendices desea registrar?`)
+  );
 
-    for (let i = 0; i < cantidadAprendiz; i++) {
-        
-        let documento = prompt("Ingrese el documento de identidad : " + (i+1))
-        let nombre = prompt("Ingrese el nombre : " + (i+1))
-        let apellidos = prompt("Ingrese los apellidos : " + (i+1))
-        let sexo = prompt("Ingrese el sexo : " + (i+1))
-        let programa = prompt("Ingrese el programa : " + (i+1))
-        let trimestre = prompt("Ingrese el trimestre : " + (i+1))
-        let direccion = prompt("Ingrese la direccion : " + (i+1))
-        let estadoPrograma = true;
+  for (let i = 0; i < cantidadAprendiz; i++) {
+    let documento = prompt("Ingrese el documento de identidad : " + (i + 1));
+    let nombre = prompt("Ingrese el nombre : " + (i + 1));
+    let apellidos = prompt("Ingrese los apellidos : " + (i + 1));
+    let sexo = prompt("Ingrese el sexo : " + (i + 1));
+    let programa = prompt("Ingrese el programa : " + (i + 1));
+    let trimestre = prompt("Ingrese el trimestre : " + (i + 1));
+    let direccion = prompt("Ingrese la direccion : " + (i + 1));
+    let estadoPrograma = true;
 
-        let datosAprendiz = {
-            documento,
-            nombre,
-            apellidos,
-            sexo,
-            programa,
-            trimestre,
-            direccion,
-            estadoPrograma,
-        }
+    let datosAprendiz = {
+      documento,
+      nombre,
+      apellidos,
+      sexo,
+      programa,
+      trimestre,
+      direccion,
+      estadoPrograma
+    };
 
-        ListadoAprendices.push(datosAprendiz)
-        
-    }
+    ListadoAprendices.push(datosAprendiz);
+  }
 }
 
-
 // Listado
-function listadoTotalAprendices(){
-    let listadoTotal = "";
-    ListadoAprendices.forEach(element => {
-        listadoTotal += 
-        `
+function listadoTotalAprendices() {
+  let listadoTotal = "";
+  ListadoAprendices.forEach((element) => {
+    listadoTotal += `
         Documento: ${element.documento}
         Nombre: ${element.nombre}
         Apellidos: ${element.apellidos}
@@ -112,44 +107,90 @@ function listadoTotalAprendices(){
         Programa: ${element.programa}
         Trimestre: ${element.trimestre}
         Dirección: ${element.direccion}
-        Estado del aprendiz: ${element.estadoPrograma ? 'APROBADO' : 'NO APROBADO'}
+        Estado del aprendiz: ${
+          element.estadoPrograma ? "APROBADO" : "NO APROBADO"
+        }
         
         -----------------------------------------------
-        `
-    });
+        `;
+  });
 
-    alert(`APRENDICES REGISTRADOS
-        ${listadoTotal}`)
+  alert(`APRENDICES REGISTRADOS
+        ${listadoTotal}`);
 }
-
 
 // Buscar Aprendices.
-function ActualizarDatosAprendiz(){
+function ActualizarDatosAprendiz() {
+  // Aprendiz por busqueda de documento
+  let posicionSeleccionada = true;
 
-    // Aprendiz por busqueda de documento
-    if(ListadoAprendices.length < 1){
-        alert("No tienes aprendices registrados")
-    }else{
-        let documento = prompt("Ingrese el documento del aprendiz a actualizar.")
-        let i = 0;
-        ListadoAprendices.forEach(element => {
-            if(documento == element.documento){
-                alert("Lo encontré "+element.nombre )
-            }
-            i++ ;
-        });
+  if (ListadoAprendices.length < 1) {
+    alert("No tienes aprendices registrados");
+  } else {
+    let documento = prompt("Ingrese el documento del aprendiz a actualizar.");
+    let i = 0;
+    let dataUsuarios = " Código  |  Documento    |   Nombre y apellidos \n";
+    ListadoAprendices.forEach((element) => {
+      if (documento == element.documento) {
+        dataUsuarios += `     ${i}    |    ${element.documento}   |   ${element.nombre} ${element.apellidos}`;
+      }
+      i++;
+    });
 
-    }
-        
+    alert(dataUsuarios);
+    // do {
+      let item = parseInt(prompt("Digite el codigo del aprendiz"));
+    //   if (item > ListadoAprendices.length) {
+    //     alert("Ese dato no existe");
+    //   } else {
+    //     posicionSeleccionada = false;
+    //   }
+    // } while (posicionSeleccionada);
 
+    do {
+      opcionActualizacionAprendices = parseInt(
+        prompt(`Actualización de datos 
+                
+                1. Documento
+                2. Nombre
+                3. Apellidos
+                4. Sexo
+                5. Programa
+                6. Trimestre
+                7. Estado
+                8. Salir`)
+      );
 
+      switch (opcionActualizacionAprendices) {
+        case 1:
+          ListadoAprendices[item].documento = prompt("Ingrese el nuevo documento")
+          break;
+        case 2:
+          ListadoAprendices[item].nombre = prompt("Ingrese el nuevo nombre")
+          break;
+        case 3:
+          ListadoAprendices[item].apellidos = prompt("Ingrese el nuevo apellido")
+          break;
+        case 4:
+          ListadoAprendices[item].sexo = prompt("Ingrese el nuevo sexo")
+          break;
+        case 5:
+          ListadoAprendices[item].programa = prompt("Ingrese el nuevo programa")
+          break;
+        case 6:
+          ListadoAprendices[item].trimestre = prompt("Ingrese el nuevo trimestre")
+          break;
+        case 7:
+          ListadoAprendices[item].estadoPrograma = prompt("Ingrese el nuevo estado")
+          break;
+        default:
+          break;
+      }
+    } while (opcionActualizacionAprendices != 8);
+
+    
+  }
 }
 
-
-
-
-
-
-
 // Arrancamos el proyecto
-start()
+start();
